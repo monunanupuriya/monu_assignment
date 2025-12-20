53456a44c78757fd55c14c12b4e1ef239ffadc8e
monunanupuriya <monunanupuriya@gmail.com>
2025-12-20 14:03:02 +0530

    Add files via upload

---
+++ infra-assignment/environment/dev/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.32.0"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "2a238ca4-a95b-43d2-846c-1618b53c6203"
+
+}
